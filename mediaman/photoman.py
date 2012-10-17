3d1e9b762d997eeaab434794b53aa65604a5329e
David Gladfelter <gladfelter@gmail.com>
2012-10-17 11:46:37 -0600

    Almost working, now I have to play with SQL and I don't want to lose what I've done.

--- mediaman/photoman.py
+++ mediaman/photoman.py
@@ -1,45 +1,214 @@
 
 import sys
 import os
+import os.path
+import glob
 import time
 import shutil
 import hashlib
 import logging
+import pyexiv2
+import datetime
 from exceptions import IOError
 from logging import StreamHandler
 from sqlite3 import dbapi2 as sqlite
 
+class Repository():
+
+  def __init__(this):
+    con = None
+
+  def open(self, lib_base_dir):  # opens, returns biggest ID or -1 on error
+    # create data store if it doesn't exist
+    if not os.access(lib_base_dir, os.R_OK|os.W_OK|os.X_OK):
+      logging.warning("can't open %s, will attempt to create it" 
+                      % lib_base_dir)
+      self._tree_setup(lib_base_dir)
+      # initialize the database
+      self.con = sqlite.connect(lib_base_dir + "/media.db")
+      cur = self.con.cursor()
+      r = cur.execute('''create table photos
+        (id integer primary key,
+        flags text,
+        md5 varchar(16),
+        constraint 'md5_UNIQUE' unique ('md5')
+        size integer,
+        description text,
+        source_info text,
+        source_path text,
+        timestamp integer)
+        ''')
+    else:  
+      self.con = sqlite.connect(lib_base_dir + "/media.db")
+    if self.con <= 0:
+      raise RuntimeError("Could not open the media database" +
+                         "for an unknown reason")
+
+  def close(self):
+    self.con.close()
+  
+  def add(self, photo):
+    cur = self.con.cursor()
+    cur.execute('''
+        insert into photos (flags, md5, size, description, source_info,
+        camera_make, camera_model, source_path, timestamp)
+        values (?, ?, ?, ?, ?, ?, ?)''',
+        (photo.flags, photo.md5, photo.size, photo.description,
+        photo.source_info, photo.camera_make, photo.camera_model,
+        photo.source_path, photo.timestamp)
+    )
+    self.con.commit()
+    return cur.lastrowid
+
+  def isHashPresent(self, md5):
+    cur = self.con.cursor()
+    rows = cur.execute('''
+        select id where md5=?''', md5)
+    return len(rows) > 0
+
+  def _tree_setup(self, lib_base_dir):
+    os.mkdir(lib_base_dir, 0755)
+    os.mkdir(lib_base_dir + '/photos', 0755)
+
+
+def openfl(path):       # open a file list, returns file object
+  return open(path, 'r')
+
+
+class Photo():
+  
+  def __init__(self, source_path):
+    self.db_id = self.flags = self.md5 = None
+    self.size = self.description = None
+    self.timestamp = self.dest_path = None
+    self.camera_make = self.camera_model = None
+    self.source_info = None
+    self.source_path = source_path
+    self.metadata_read = False
+    pass
+
+  def get_path_parts(self):
+    if not self.metadata_read:
+      self.load_metadata()
+    time_struct = time.localtime(self.timestamp)
+    return time_struct[0:2] + (os.path.basename(self.source_path),)
  
-def store_open(dataloc):  # opens, returns biggest ID or -1 on error
-  # create data store if it doesn't exist
-  if not os.access(dataloc, os.R_OK|os.W_OK|os.X_OK):
-    logging.warning("can't open %s, will attempt to create it" % dataloc)
-    tree_setup()
-    # initialize the database
-    con = sqlite.connect(dataloc + "/media.db")
-    cur = con.cursor()
-    r = cur.execute('''create table photos
-      (id integer primary key,
-      flags text,
-      md5 varchar(16),
-      constraint 'md5_UNIQUE' unique ('md5')
-      size integer,
-      description text,
-      source_info text,
-      source_path text,
-      timestamp integer)
-      ''')
-  else:  
-    con = sqlite.connect(dataloc + "/media.db")
-  if con > 0:
-    return con
-  else:
-    return -1
+  def load_metadata(self):
+    m = None
+    time_parts = None
+    try:
+      m = pyexiv2.ImageMetadata(self.source_path)
+      m.read()
+    except IOError:
+      m = None
+      logging.warning("%s contains no EXIF data" % self.source_path)
+    except Exception:
+      m = None
+      logging.warning('An unexpected error occurred while reading '
+                      + 'exif data from %s. Will attempt to recover')
+
+    if m is not None:
+      image_keys = frozenset(m.exif_keys)
+      self._load_exif_timestamp(m, image_keys)
+      self._load_camera_make(m, image_keys)
+      self._load_camera_model(m, image_keys)
+
+    self._load_filesystem_timestamp()
+    self._load_file_size()
+    self.md5 = self.get_hash()
+    self.metadata_read = True
+
+  def _load_file_size(self):
+    try:
+      self.size = os.path.getsize(self.source_path)
+    except os.error:
+      self.size = 0
+      logging.warning('Could not read file size of %s' % self.source_path)
+
+  def _load_camera_model(self, metadata, image_keys):
+    if 'Exif.Image.Model' in image_keys:
+      self.camera_model = metadata['Exif.Image.Model'].value
+
+  def _load_camera_make(self, metadata, image_keys):
+    if 'Exif.Image.Make' in image_keys:
+      self.camera_make = metadata['Exif.Image.Make'].value
+
+  def _load_exif_timestamp(self, metadata, image_keys):
+    time_keys = ['Exif.Image.DateTimeOriginal', 'Exif.Photo.DateTime',
+                 'Exif.Photo.DateTimeDigitized']
+    for key in time_keys:
+      if key in image_keys:
+        try:
+          time_struct = metadata[key].value.timetuple()
+          self.timestamp = time.mktime(time_struct)
+          break
+        except ValueError:
+          pass
+
+  def _load_filesystem_timestamp(self):
+    if self.timestamp is None:
+      try:
+        self.timestamp = os.path.getmtime(self.source_path)
+      except os.error:
+        logging.warning('Could not access image''s timestamp' +
+                        ' by any means, setting it to the epoch start.')
+        self.timestamp = 0
+      
+  def get_hash(self):
+    fo = open(self.source_path, 'r')
+    m = hashlib.md5()
+    stuff = fo.read(8192)
+    while len(stuff) > 0:
+            m.update(stuff)
+            stuff = fo.read(8192)
+    fo.close()
+    return (m.hexdigest())
 
 
-def tree_setup(dataloc):
-  os.mkdir(dataloc, 0755)
-  os.mkdir(dataloc + '/photos', 0755)
+def read_photos(search_dir, lib_base_dir):
+  r = Repository()
+  r.open(lib_base_dir)
+  photos = os.listdir(search_dir)
+  logging.info('Found these photo files: %s' % photos)
+  for path in photos:
+    path = os.path.join(search_dir, path)
+    if os.path.isfile(path):
+      photo = Photo(path)
+      photo.load_metadata()
+      copy_photo(photo, lib_base_dir)
+      photo.db_id = r.add(photo)
+      if photo.db_id > 0 and os.path.isfile(photo.dest_path):
+        dest_photo = Photo(photo.dest_path)
+        dest_photo.load_metadata()
+        if dest_photo.md5 is not None and dest_photo.md5 == photo.md5:
+          logging.info('%s was successfully copied to destination %s, ' +
+                       'deleting the source file %s.'
+                       % (photo.source_path, photo.dest_path))
+          os.remove(photo.source_path)
+        else:
+          logging.warning('Destination photo file %s didn''t match ' +
+                          'the hash of or wasn''t properly transferred ' +
+                          'from %s' % (photo.dest_path, photo.source_path))
+      else:
+        logging.warning('%s was not copied to %s or it failed to be ' +
+                        'inserted into the database, skipping deletion ' +
+                        'of the original'
+                        % (photo.source_path, photo.dest_path))
+    else:
+      logging.warning('Found a non-file when looking for photos: %s, ' +
+                      'it will not be modified' % (path,))
+
+
+def copy_photo(photo, lib_base_dir):
+  relative_path = "%04d/%02d/%s" % photo.get_path_parts()
+  dest_path = os.path.join(lib_base_dir, relative_path)
+  photo.dest_path = dest_path
+  dest_dir = os.path.dirname(photo.dest_path)
+  if not os.path.exists(dest_dir):
+    print 'Creating directory %s' % dest_dir
+    os.makedirs(dest_dir)
+  print 'copying from %s to %s' % (photo.source_path, photo.dest_path)
+  shutil.copy(photo.source_path, photo.dest_path)
 
 
 """Configures logging to stderr, file.
@@ -48,12 +217,11 @@ def tree_setup(dataloc):
 def configure_logging():
   root = logging.getLogger('')
   h = logging.StreamHandler(sys.stderr)
-  formatter = logging.Formatter('%(asctime)s %(filename)s:%(lineno)d %(levelname)s %(message)s')
+  formatter = logging.Formatter('%(asctime)s %(filename)s' +
+                                ':%(lineno)d %(levelname)s %(message)s')
   h.setFormatter(formatter)
   root.addHandler(h)
-  fh = logging.FileHandler('/var/tmp/myapp.log')
+  fh = logging.FileHandler('/var/tmp/photoman.log')
   fh.setFormatter(formatter)
   root.addHandler(fh)
   root.setLevel(logging.WARNING)
-
-
--- mediaman/photoman_test.py
+++ mediaman/photoman_test.py
@@ -3,24 +3,30 @@ import unittest
 from mock import *
 import sqlite3
 import os
+import time
 import photoman
+import pyexiv2
+import tempfile
+import glob
+import shutil
 
 
-class TestPhotoMan(unittest.TestCase):
+class TestRepository(unittest.TestCase):
 
   def setUp(self):
+    self.rep = photoman.Repository()
     pass
 
   @patch('sqlite3.dbapi2.connect')
   @patch('os.access')
-  @patch('photoman.tree_setup')
-  def test_store_open(self, tree_setup, access, connect):
+  @patch('photoman.Repository._tree_setup')
+  def test_open(self, tree_setup, access, connect):
     access.return_value = True
     conn_mock = Mock()
     cur_mock = Mock()
     conn_mock.cursor.return_value = cur_mock()
     connect.return_value = conn_mock
-    photoman.store_open('/tmp/bar')
+    self.rep.open('/tmp/bar')
     access.assert_called_with('/tmp/bar', ANY)
     connect.assert_called_with('/tmp/bar/media.db')
     self.assertFalse(conn_mock.cursor.called,
@@ -30,14 +36,14 @@ class TestPhotoMan(unittest.TestCase):
 
   @patch('sqlite3.dbapi2.connect')
   @patch('os.access')
-  @patch('photoman.tree_setup')
-  def test_store_create(self, tree_setup, access, connect):
+  @patch('photoman.Repository._tree_setup')
+  def test_create(self, tree_setup, access, connect):
     access.return_value = False
     conn_mock = Mock(name="connection_mock", spec_set=['cursor'])
     cur_mock = Mock(name="cursor_mock", spec_set=['execute'])
     conn_mock.cursor.return_value = cur_mock
     connect.return_value = conn_mock
-    photoman.store_open('/tmp/bar')
+    self.rep.open('/tmp/bar')
     access.assert_called_with('/tmp/bar', ANY)
     connect.assert_called_with('/tmp/bar/media.db')
     self.assertTrue(conn_mock.cursor.called,
@@ -49,9 +55,120 @@ class TestPhotoMan(unittest.TestCase):
 
   @patch('os.mkdir')
   def test_tree_setup(self, mkdir):
-    photoman.tree_setup('/tmp/foo')
+    photoman.Repository()._tree_setup('/tmp/foo')
     self.assertEquals(2, mkdir.call_count)
 
+  
+  def test_add(self):
+    rep = photoman.Repository()
+    photo = Mock()
+    rep.con = Mock()
+    rep.con.cursor.return_value.lastrowid = 42
+    self.assertEquals(42, rep.add(photo))
+    self.assertTrue(rep.con.cursor.called)
+    self.assertTrue(rep.con.cursor.return_value.execute.called)
+    self.assertTrue(rep.con.commit.called)
+
+
+class TestPhoto(unittest.TestCase):
+
+  def setUp(self):
+    self.photo = photoman.Photo('/tmp/foo.jpg')
+    self.time_struct = time.strptime('2012-08-19 15:14:04', '%Y-%m-%d %H:%M:%S')
+    self.timestamp = time.mktime(self.time_struct)
+    pass
+
+  @patch('pyexiv2.ImageMetadata')
+  def test_get_path_parts(self, image):
+    self.photo.metadata_read = True
+    self.photo.timestamp = self.timestamp
+    self.assertEquals((2012, 8, 'foo.jpg'), self.photo.get_path_parts())
+
+  @patch('pyexiv2.ImageMetadata')
+  def test_get_path_parts_uninitialized(self, image):
+    self.photo.metadata_read = False
+    with patch.object(self.photo, 'load_metadata') as ts:
+      self.photo.timestamp = self.timestamp
+      self.assertEquals((2012, 8, 'foo.jpg'), self.photo.get_path_parts())
+      self.assertTrue(self.photo.load_metadata.called)
+
+  @patch('pyexiv2.ImageMetadata')
+  def test_load_metadata(self, image):
+    instance = image.return_value
+    with patch.object(self.photo, '_load_exif_timestamp') as ts:
+      with patch.object(self.photo, '_load_camera_make') as cmake:
+        with patch.object(self.photo, '_load_camera_model') as cmodel:
+          with patch.object(self.photo, '_load_file_size') as fs:
+            with patch.object(self.photo, '_load_filesystem_timestamp') as fts:
+              with patch.object(self.photo, 'get_hash') as fts:
+                self.photo.load_metadata()
+                self.assertTrue(ts.called)
+                self.assertTrue(self.photo._load_camera_make.called)
+                self.assertTrue(self.photo._load_camera_model.called)
+                self.assertTrue(self.photo._load_file_size.called)
+
+  def test_load_exif_timestamp(self):
+    m = MagicMock(spec_set=['__getitem__'])
+    image_keys = ['Exif.Image.DateTimeOriginal']
+    m.__getitem__.return_value.value = '2012-08-19 15:14:04'
+    self.photo._load_exif_timestamp(m, image_keys)
+    self.assertEquals(self.timestamp, self.photo.timestamp)
+
+  @patch('os.path.getmtime')
+  def test_load_filesystem_timestamp(self, getmtime):
+    getmtime.return_value = self.timestamp
+    self.photo._load_filesystem_timestamp()
+    self.assertEquals(self.timestamp, self.photo.timestamp)
+
+  def test_load_camera_make(self):
+    m = MagicMock(spec_set=['__getitem__'])
+    image_keys = ['Exif.Image.Make']
+    m.__getitem__.return_value.value = 'Foo'
+    self.photo._load_camera_make(m, image_keys)
+    self.assertEquals('Foo', self.photo.camera_make)
+
+  def test_load_camera_model(self):
+    m = MagicMock(spec_set=['__getitem__'])
+    image_keys = ['Exif.Image.Model']
+    m.__getitem__.return_value.value = 'Bar'
+    self.photo._load_camera_model(m, image_keys)
+    self.assertEquals('Bar', self.photo.camera_model)
+
+class PhotoManFunctionalTests(unittest.TestCase):
+
+  def testNewDatabase(self):
+    scriptdir = os.path.dirname(os.path.realpath(__file__))
+    test_data_dir = os.path.join(scriptdir, 'test')
+    tmpdir = tempfile.mkdtemp()
+    try:
+      srcdir = os.path.join(tmpdir, 'src')
+      destdir = os.path.join(tmpdir, 'dest')
+      mediadir = os.path.join(destdir, 'media')
+      os.mkdir(srcdir)
+      os.mkdir(destdir)
+      test_files = glob.glob(os.path.join(test_data_dir, '*.jpg'))
+      test_files += glob.glob(os.path.join(test_data_dir, '*.JPG'))
+      for test_file in test_files:
+        shutil.copy(test_file, srcdir)
+      photoman.read_photos(srcdir, mediadir)
+      self._printTree(tmpdir)
+    finally:
+      shutil.rmtree(tmpdir)
+
+  def _printTree(self, basedir):
+    print basedir + '/'
+    dirs = []
+    for obj in os.listdir(basedir):
+      full_path = os.path.join(basedir, obj)
+      if os.path.isfile(full_path):
+        print os.path.join(basedir, full_path)
+      elif os.path.isdir(full_path):
+        dirs.append(full_path)
+    for dirname in dirs:
+      self._printTree(dirname)
+
+
+      
 
 if __name__ == '__main__':
   unittest.main()
