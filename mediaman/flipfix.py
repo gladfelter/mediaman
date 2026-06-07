71cfd0c384a6e7aa557a49143c9479c009a63a44
David Gladfelter <gladfelter@users.noreply.github.com>
2026-06-07 12:14:42 -0600

    Fix flipfix.py: restore correct Python 3 version after merge conflict

--- mediaman/flipfix.py
+++ mediaman/flipfix.py
@@ -1,20 +1,17 @@
-#! /usr/bin/python
-#
+#!/usr/bin/env python3
 """Data fix for bad date on Flip camera"""
-
 import calendar
 import os
 import time
-from stat import *
-
 
 files = os.listdir('.')
 for filepath in files:
-  filestat = os.stat(filepath)
-  mstruct_time = time.gmtime(filestat[ST_MTIME])
-  atime = filestat[ST_ATIME]
-  mstruct_time_list = list(mstruct_time)
-  mstruct_time_list[0] += 1
-  mtime = calendar.timegm(time.struct_time(mstruct_time_list))
-  print filepath, time.ctime(atime), time.ctime(mtime)
-  os.utime(filepath, (atime, mtime))
+    filestat = os.stat(filepath)
+    mtime = filestat.st_mtime
+    atime = filestat.st_atime
+    mstruct_time = time.gmtime(mtime)
+    mstruct_time_list = list(mstruct_time)
+    mstruct_time_list = (mstruct_time_list[0] + 1,) + mstruct_time_list[1:]
+    new_mtime = calendar.timegm(time.struct_time(mstruct_time_list))
+    print(filepath, time.ctime(atime), time.ctime(new_mtime))
+    os.utime(filepath, (atime, new_mtime))
